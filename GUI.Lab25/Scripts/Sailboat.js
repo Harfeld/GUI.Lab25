cb533a2e7c46336b631375595c06bd0dfc122cff
Andreas <201608930@post.au.dk>
2019-04-30 09:50:25 +0200

    Det virker ikke men det er da sat op

---
+++ GUI.Lab25/Scripts/Sailboat.js
@@ -0,0 +1,17 @@
+﻿class Sailboat {
+    constructor(name, length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    get speed() {
+        return this.Hullspeed();
+    }
+    Hullspeed() {
+        return 1.34 * Math.sqrt(this.length);
+    }
+}
+
+function btn_clk(nm, ln) {
+    let boat = new Sailboat(nm, ln);
+}
